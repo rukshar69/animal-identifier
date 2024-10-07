081894471ec91a1fc07c7f4daa2c4044f182496b
rukshar69 <ruksharalam7@gmail.com>
2024-10-08 00:48:47 +0600

    .

--- fastapi-docker/app.py
+++ fastapi-docker/app.py
@@ -1,14 +1,13 @@
-# create a fastapi get api that will return a json containing a string hello world
-
-from fastapi import FastAPI
-import google.generativeai as genai #https://ai.google.dev/pricing
-from dotenv import load_dotenv
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 import os
-import PIL.Image
+from PIL import Image
+import google.generativeai as genai
+from dotenv import load_dotenv
 
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
 # Access the API_KEY environment variable
 api_key = os.getenv("API_KEY")
@@ -17,18 +16,26 @@ genai.configure(api_key=api_key)
 
 app = FastAPI()
 
-def img_analysis():
+def img_analysis(image):
     model = genai.GenerativeModel(
         "gemini-1.5-flash",
-        system_instruction="You are expert animal identifier. Given an image of"\
-            "an animal, tell me the name of the animal in the image",)
-    animal_img = PIL.Image.open( "images/tiger.jpg")
-    response = model.generate_content([animal_img, "What is the name of the animal in the image?"], max_tokens=10)
-    print(response)
+        system_instruction="You are an expert animal identifier. Given an image of an animal, tell me the name of the animal in the image."\
+        "If the image is not an animal, output 'not animal'. Otherwise, output the name of the animal only. Do not output any other text or symbols or newline.",
+    )
+    response = model.generate_content([image, "What is the name of the animal in the image?"])
     return response.text
 
 
 @app.get("/")
 async def read_root():
-    response = img_analysis()
+    response = "Animal Detector"
     return {"Response": response}
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    try:
+        image = Image.open(file.file)
+        response = img_analysis(image)
+        return {"Response": response}
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"error": str(e)})
